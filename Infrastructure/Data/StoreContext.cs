e9f6ba25b0617c4b9c0a5cd3380202e4a351b216
dat-tnm <tnmdat183@gmail.com>
2023-08-11 23:41:35 +0700

    initial commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
